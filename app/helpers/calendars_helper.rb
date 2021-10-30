8c08b5bc93682e1007883a9b8a178ddbe12cae50
syumpei1997 <syumpei1997@gmail.com>
2021-10-30 21:28:12 +0900

    Revert "lssue1~3の修正"
    This reverts commit 9efb57c1a939ec9f28c95f76d21b6800f00597cc.

--- app/helpers/calendars_helper.rb
+++ app/helpers/calendars_helper.rb
@@ -1,2 +1,9 @@
 module CalendarsHelper
+  def weeks_day_lists(day)
+    html = ''
+    days.each do |day|
+      html += <div class='days'>
+      def class='date'> day[:month] %>/<% day[:date] %>
+      end
+      return raw(html)
 end
